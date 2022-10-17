c3d4e487f711342827bd756db591448943a51f0c
xPsyBit <priyam7911@gmail.com>
2022-10-18 04:57:33 +0530

    Theme Switching -  Added a button which switches dark and light theme   - Does not store theme selection (by design)   - Picks system theme on reload - **NOTE:** Switched icons to [Iconify](https://iconify.design/)

---
+++ scripts/theme.js
@@ -0,0 +1,43 @@
+function loadMqTheme() {
+    const lightThemeMq = window.matchMedia('(prefers-color-scheme: light)');
+    if (lightThemeMq.matches) {
+        setTheme("light");
+    } else {
+        setTheme("dark");
+    }
+    lightThemeMq.addEventListener('change', (event) => {
+        if (event.matches) {
+            setTheme("light");
+        } else {
+            setTheme("dark");
+        }
+    });
+}
+
+function switchTheme() {
+    var themeElementClass = document.documentElement.classList;
+    if (themeElementClass.contains("light-theme")) {
+        setTheme("dark");
+    } else {
+        console.log("test");
+        setTheme("light");
+    }
+}
+
+function setTheme(theme) {
+    var themeElementClass = document.documentElement.classList;
+    var themeSwitchClass = document.getElementById("theme-button")?.classList;
+    if (theme == "light") {
+        themeElementClass.remove("dark-theme");
+        themeSwitchClass?.remove("dark-theme");
+        themeElementClass.add("light-theme");
+        themeSwitchClass?.add("light-theme");
+    } else if (theme == "dark") {
+        themeElementClass.remove("light-theme");
+        themeSwitchClass?.remove("light-theme");
+        themeElementClass.add("dark-theme");
+        themeSwitchClass?.add("dark-theme");
+    } else {
+        console.log("error in setting theme");
+    }
+}
