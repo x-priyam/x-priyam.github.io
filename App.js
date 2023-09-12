b183ee823705291e6a2a25c270ba9d4a83afa125
xPsyBit <priyam7911@gmail.com>
2023-09-12 17:18:02 +0530

    Icon widths - stopped using font-size to set the size of icons   - now using width and height   - allowed by setting height to unset in html - Made animation for theme-switch button more user friendly

--- App.js
+++ App.js
@@ -1,32 +1,13 @@
 function setThemeIcon() {
   let themeIcon = document.querySelector("#theme-switch")
-  if (document.documentElement.classList.contains("light-theme")) {
-    themeIcon.setAttribute("icon", "tabler:sun-filled");
-  } else {
-    themeIcon.setAttribute("icon", "tabler:moon-filled");
-  }
-  return;
-}
-
-// CSS doesn't allow font size to be set relative to parent width
-// we set width in CSS and then copy the computed value to font size
-// useful when using icons from a font as images
-function setIconSizes() {
-  document.querySelectorAll("iconify-icon:not(.top-container *)").forEach((fontIcon) => {
-    fontIcon.style.setProperty(
-      "font-size",
-      getComputedStyle(fontIcon).getPropertyValue("width")
-    )
-  });
-  document.querySelectorAll(".top-container iconify-icon").forEach((fontIcon) => {
-    let width = getComputedStyle(fontIcon).getPropertyValue("width")
-    let height = getComputedStyle(fontIcon).getPropertyValue("height")
-
-    fontIcon.style.setProperty(
-      "font-size",
-      (Number(width.slice(0, -2)) < Number(height.slice(0, -2))) ? width : height
-    )
-  });
+  document.startViewTransition(() => {
+    if (document.documentElement.classList.contains("light-theme")) {
+      themeIcon.setAttribute("icon", "tabler:sun-filled");
+    } else {
+      themeIcon.setAttribute("icon", "tabler:moon-filled");
+    }
+  })
+  themeIcon.classList.add("bounce-animation")
   return;
 }
 
@@ -36,17 +17,10 @@ window.addEventListener("load", () => {
     document.documentElement.classList.add("light-theme");
   }
   setThemeIcon();
-  setIconSizes();
-});
-
-window.addEventListener("resize", () => {
-  setIconSizes();
 });
 
 // change theme on click of theme-switch button
 document.querySelector("#theme-switch").addEventListener("click", () => {
   document.documentElement.classList.toggle("light-theme");
-  document.startViewTransition(() => {
-    setThemeIcon();
-  });
+  setThemeIcon();
 });
